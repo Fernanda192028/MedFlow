79f3198a95147c15a6101b9e6d740417cd1800a8
Fernanda Cassiano <fernandafcbpereira19071@gmail.com>
2026-05-02 14:31:38 -0300

    fix: ajusta validação e redirecionamento do login

--- js/main.js
+++ js/main.js
@@ -1,58 +1,67 @@
 // Função 1 - Menu Responsivo
 
-$(document).ready(function() {
-    $('.fa-bars').click(function() {
-       $(this).toggleClass('fa-times')
-       $('.navbar').toggleClass('nav-toggle')
+$(document).ready(function () {
+    $('.fa-bars').click(function () {
+        $(this).toggleClass('fa-times')
+        $('.navbar').toggleClass('nav-toggle')
     })
 
-// Função 2 - Scroll Efeito
-    $(window).on('scroll load', function() {
+    // Função 2 - Scroll Efeito
+    $(window).on('scroll load', function () {
         $('.fa-bars').removeClass('fa-times')
         $('.navbar').removeClass('nav-toggle')
 
-        if($(window).scrollTop() > 30) {
+        if ($(window).scrollTop() > 30) {
             $('header').addClass('header-active')
         } else {
             $('header').removeClass('header-active')
         }
 
- })
+    })
 });
 
 //Função 3 - Abrir Login
 
-$(document).ready(function() {
-    $('.login').click(function() {
+$(document).ready(function () {
+    $('.login').click(function () {
         window.location.href = "login.html";
     });
 });
 
-   // Função 4 - Login 
-    $('#loginForm').submit(function(e) {
+
+// Função 4 - Validação de Login
+$(document).ready(function () {
+
+    $('#loginForm').submit(function (e) {
         e.preventDefault();
 
         let email = $('#email').val().trim().toLowerCase();
         let senha = $('#senha').val().trim();
-        let mensagem = $('#loginFeedback');
 
-        mensagem.text('').css('color', 'red');
+
+        if (email === "" || senha === "") {
+            alert("Preencha todos os campos!");
+            return;
+        }
+
 
         if (email === "medico@gmail.com" && senha === "medico123") {
-            mensagem.css('color', 'green').text("Bem-vindo, médico!");
+            alert("Login de médico realizado!");
+            window.location.href = "disponibilidade.html";
 
-            setTimeout(function() {
-                window.location.href = "disponibilidade.html";
-            }, 800);
 
         } else if (email === "paciente@gmail.com" && senha === "paciente123") {
-            mensagem.css('color', 'green').text("Bem-vindo, paciente!");
-
-            setTimeout(function() {
-                window.location.href = "agendamento.html";
-            }, 800);
+            alert("Login de paciente realizado!");
+            window.location.href = "agendamento.html";
 
         } else {
-            mensagem.text("Email ou senha incorretos!");
+            alert("Email ou senha incorretos!");
+
+            $('#email').val('');
+            $('#senha').val('');
+
+
+            $('#email').focus();
         }
-    });
+    }
+    )});
