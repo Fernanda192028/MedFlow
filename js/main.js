f5f20919f64741e5f9c983f11addc1b74f5f0938
Fernanda Cassiano <fernandafcbpereira19071@gmail.com>
2026-05-02 12:24:11 -0300

    feat: adiciona página de login

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
         $('.fa-bars').removeClass('fa-times')
         $('.navbar').removeClass('nav-toggle')
 
-        if($(Window).scrollTop() > 30) {
+        if($(window).scrollTop() > 30) {
             $('header').addClass('header-active')
         } else {
             $('header').removeClass('header-active')
@@ -28,3 +28,31 @@ $(document).ready(function() {
     });
 });
 
+   // Função 4 - Login 
+    $('#loginForm').submit(function(e) {
+        e.preventDefault();
+
+        let email = $('#email').val().trim().toLowerCase();
+        let senha = $('#senha').val().trim();
+        let mensagem = $('#loginFeedback');
+
+        mensagem.text('').css('color', 'red');
+
+        if (email === "medico@gmail.com" && senha === "medico123") {
+            mensagem.css('color', 'green').text("Bem-vindo, médico!");
+
+            setTimeout(function() {
+                window.location.href = "disponibilidade.html";
+            }, 800);
+
+        } else if (email === "paciente@gmail.com" && senha === "paciente123") {
+            mensagem.css('color', 'green').text("Bem-vindo, paciente!");
+
+            setTimeout(function() {
+                window.location.href = "agendamento.html";
+            }, 800);
+
+        } else {
+            mensagem.text("Email ou senha incorretos!");
+        }
+    });
